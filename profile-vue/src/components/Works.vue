2ea1f07885967a559ed92c9160540786274b1d65
namatame-karaage <mariypuuu0519@gmail.com>
2020-10-22 14:02:22 +0900

    update/reademe

--- profile-vue/src/components/Works.vue
+++ profile-vue/src/components/Works.vue
@@ -5,8 +5,28 @@
       <p class="works-subtitle">*随時、追加します</p>
       <div class="works-box">
         <div class="works-card">
+          <img src="../assets/images/myprofile.png" alt="myprofile" class="works-img" />
+          <h4>MyProfile</h4>
+          <p class="works-text">簡単な自己紹介です。</p>
+          <p class="works-made">制作期間: ４日間</p>
+          <p><span class="type-btn">vue.js</span></p>
+          <a href="https://github.com/namatame-karaage/myprofile.vue/tree/master/profile-vue"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a>
+        </div>
+         <div class="works-card">
+          <img src="../assets/images/raght.png" alt="myprofile" class="works-img" />
+          <h4>ある料理店</h4>
+          <p class="works-text">コーポレイト</p>
+          <p class="works-made">制作期間: ４日間</p>
+          <p><span class="type-btn">Scss</span>
+          <p><span class="type-btn">Javascirpt</span></p>
+          <a href="https://cook-ragut-due.netlify.app/"><font-awesome-icon icon="sign-in-alt" class="icon-home"/></a>
+          <a href="https://github.com/namatame-karaage/basic-html-css-js"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a>
+        </div>
+        <div class="works-card">
           <img src="../assets/images/rails.animal.png" alt="animal.portfolio" class="works-img"/>
           <h4>Animals Domi</h4>
+          <p class="works-text">動物・環境のニュース</p>
+          <p class="works-made">制作期間: 14日間</p>
             <p>
               <span class="type-btn">Rails</span>
               <span class="type-btn">Scss</span>
@@ -20,18 +40,24 @@
           <a href="https://roteger-les-animaux.herokuapp.com/"><font-awesome-icon icon="sign-in-alt" class="icon-home"/></a>
           <a href="https://github.com/namatame-karaage/animals-dona"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a>
         </div>
-
         <div class="works-card">
-          <img src="../assets/images/myprofile.png" alt="myprofile" class="works-img" />
-          <h4>MyProfile</h4>
-          <p><span class="type-btn">vue.js</span></p>
-          <a href="https://github.com/namatame-karaage/myprofile.vue/tree/master/profile-vue"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a>
-        </div>
-        <div class="works-card">
-          <img src="../assets/images/noimage.png" alt="myprofile" class="works-img" />
-          <!-- <h4>作成中</h4> -->
-          <p><span class="type-btn">作成中</span></p>
-          <!-- <a href="https://github.com/namatame-karaage/myprofile.vue/tree/master/profile-vue"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a> -->
+          <img src="../assets/images/noimage.png" alt="animal.portfolio" class="works-img"/>
+          <h4>Vue.jsで作成中</h4>
+          <p class="works-text">Vue.routerなど使用する予定</p>
+          <!-- <p class="works-text">動物・環境のニュース</p>
+          <p class="works-made">制作期間: 14日間</p>
+            <p>
+              <span class="type-btn">Rails</span>
+              <span class="type-btn">Scss</span>
+              <span class="type-btn">Javascirpt</span>
+            </p>
+            <p>
+              <span class="type-btn">Aws(s３)</span>
+              <span class="type-btn">TravisCi</span>
+              <span class="type-btn">Docker</span>
+            </p> -->
+          <!-- <a href="https://roteger-les-animaux.herokuapp.com/"><font-awesome-icon icon="sign-in-alt" class="icon-home"/></a>
+          <a href="https://github.com/namatame-karaage/animals-dona"><font-awesome-icon :icon="['fab','github']" class="icon-github"/></a> -->
         </div>
       </div>
     </div>
@@ -87,7 +113,12 @@
   transform: scale(1.04);
   opacity: .9;
     box-shadow: 0 10px 10px rgba(0, 0, 0, 0.4);
+}
 
+.works-text,
+.works-made{
+  font-size: 13px;
+  color: #333
 }
 
 .works-img {
